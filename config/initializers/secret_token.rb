9af502bf49f9b3af6aef8266bf5033fa1ce7a3ab
sambit11 <sambit11@outlook.com>
2013-11-13 15:46:01 +0530

    proj commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GithubHostingTest::Application.config.secret_token = '44ca35c48c60544c0fca7a63ddaf7445c04513f78325cf8802c640a5b2c535f0de3fc8605d88fe1a57c9a394397ced168e89a422c7ebdb32a5cb097085da45e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GithubHostingTest::Application.config.session_store :cookie_store, key: '_GithubHostingTest_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GithubHostingTest::Application.config.session_store :active_record_store
